3fada984ae1f8b40150cf236cbcf662247ec0a01
DevanshuTripathi120 <aditheprince120@gmail.com>
2025-08-13 15:00:33 +0530

    chors: firebaseConfig chors: React router setup with pages folder

---
+++ firebaseConfig.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyChMXBHOQrvDav6-apozGv5C1RaeMF7fSQ",
+  authDomain: "codepvp-56d75.firebaseapp.com",
+  projectId: "codepvp-56d75",
+  storageBucket: "codepvp-56d75.firebasestorage.app",
+  messagingSenderId: "47075937133",
+  appId: "1:47075937133:web:8e073ba83083563283e864",
+  measurementId: "G-QD36SMWXY9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default app;
