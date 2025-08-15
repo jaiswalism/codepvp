381d11d7a5777fa2bd0a70e90ef52579da1b26ef
DevanshuTripathi <aditheprince120@gmail.com>
2025-08-15 13:17:31 +0530

    feat: added a login page but no links to it will add it later

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
     return(
-        <div>
+        <div className="h-dvh w-dvw flex justify-center items-center" >
             <Outlet />
         </div>
     );
