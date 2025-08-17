5be124a5e22b4dc69f3a7ca129608f8aa9a02050
DevanshuTripathi <aditheprince120@gmail.com>
2025-08-17 13:58:22 +0530

    feat: the problem page is not static anymore

--- src/pages/Problem.tsx
+++ src/pages/Problem.tsx
@@ -4,15 +4,34 @@ import { editor } from 'monaco-editor'
 import { useParams } from 'react-router-dom';
 import { db } from '../../firebaseConfig';
 import { doc, getDoc } from 'firebase/firestore';
+import { useState } from 'react';
+
+interface ProblemData {
+    title: string;
+    stmt: string;
+    testcases: {
+            stdin: string;
+            expected_output: string;
+            hidden: boolean;
+            display: {
+                input: string;
+                output: string
+            }
+    }[];
+    constraints: string[];
+}
 
 const Problem: React.FC = () => {
 
     const { problemId } = useParams<{ problemId: string }>();
 
+    const [data, setData] = useState<ProblemData | null>(null)
+
     useEffect(() => {
         if(problemId) {
             console.log(problemId);
             getDocumentData("problems", problemId);
+            console.log(data);
         }
     }, [problemId])
 
@@ -20,7 +39,7 @@ const Problem: React.FC = () => {
         const docRef = doc(db, collectionName, documentId);
         const docSnap = await getDoc(docRef);
         if(docSnap.exists()) {
-            console.log(docSnap.data());
+            setData(docSnap.data() as ProblemData);
         } else {
             console.log("GAY")
         }
@@ -45,7 +64,7 @@ const Problem: React.FC = () => {
       
       {/* Header */}
       <header className="flex justify-between items-center p-4 border-b border-gray-700/50">
-        <h2 className="text-2xl font-bold text-cyan-300">Two Sum</h2>
+        <h2 className="text-2xl font-bold text-cyan-300">{ data?.title }</h2>
         {/* <button className="text-purple-300 hover:text-white transition-colors duration-300 text-lg flex items-center gap-2">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
           Back to Topics
@@ -58,34 +77,41 @@ const Problem: React.FC = () => {
         <div className="w-1/2 p-6 overflow-y-auto">
           <h3 className="text-xl font-bold text-white mb-4">Problem Statement</h3>
           <p className="text-gray-300 mb-6">
-            Given an array of integers `nums` and an integer `target`, return indices of the two numbers such that they add up to `target`.
-            You may assume that each input would have exactly one solution, and you may not use the same element twice.
-            You can return the answer in any order.
+            { data?.stmt }
           </p>
-
-          <h3 className="text-xl font-bold text-white mb-4">Example 1</h3>
+            {data?.testcases.filter(tc => !tc.hidden).map((tc, i) => (
+                <div key={i}>
+                <h3 className="text-xl font-bold text-white mb-4">Example {i + 1}</h3>
+                    <div className="bg-gray-900/50 p-4 rounded-lg mb-6">
+                        <code className="text-gray-300">
+                        <span className="text-purple-400">Input:</span> { tc.display.input }<br/>
+                        <span className="text-purple-400">Output:</span> { tc.display.output }<br/>
+                        </code>
+                    </div>
+                </div>
+            ))}
+          {/* <h3 className="text-xl font-bold text-white mb-4">Example 1</h3>
           <div className="bg-gray-900/50 p-4 rounded-lg mb-6">
             <code className="text-gray-300">
-              <span className="text-purple-400">Input:</span> nums = [2, 7, 11, 15], target = 9<br/>
+              <span className="text-purple-400">Input:</span> { data?.testcases.display.input }<br/>
               <span className="text-purple-400">Output:</span> [0, 1]<br/>
               <span className="text-purple-400">Explanation:</span> Because nums[0] + nums[1] == 9, we return [0, 1].
             </code>
-          </div>
+          </div> */}
 
-          <h3 className="text-xl font-bold text-white mb-4">Example 2</h3>
+          {/* <h3 className="text-xl font-bold text-white mb-4">Example 2</h3>
           <div className="bg-gray-900/50 p-4 rounded-lg mb-6">
             <code className="text-gray-300">
               <span className="text-purple-400">Input:</span> nums = [3, 2, 4], target = 6<br/>
               <span className="text-purple-400">Output:</span> [1, 2]
             </code>
-          </div>
+          </div> */}
 
           <h3 className="text-xl font-bold text-white mb-4">Constraints</h3>
           <ul className="list-disc list-inside text-gray-300 space-y-2">
-            <li>{`2 <= nums.length <= 10^4`}</li>
-            <li>{`-10^9 <= nums[i] <= 10^9`}</li>
-            <li>{`-10^9 <= target <= 10^9`}</li>
-            <li>Only one valid answer exists.</li>
+            {data?.constraints.map((c, i) => (
+                <li key={i}>{ c }</li>
+            ))}
           </ul>
         </div>
 
