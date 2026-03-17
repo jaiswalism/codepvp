4f998311e5f9c1a4bc967e647d61f01996ea4903
DevanshuTripathi <aditheprince120@gmail.com>
2026-03-17 10:29:43 +0530

    demo cghange

--- src/pages/FrontendQueue.tsx
+++ src/pages/FrontendQueue.tsx
@@ -3,7 +3,7 @@ import LoadingScreen from "./components/LoadingScreen"
 export default function FrontendQueue(){
     return(
         <div>
-            <LoadingScreen message="Finding Players" />
+            <LoadingScreen message="Searchin for players" />
         </div>
     )
 }
